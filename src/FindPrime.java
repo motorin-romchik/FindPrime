fcc6bdd13c9d598bbfece73dfa6de271259c803a
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-03-30 23:07:15 +0700

    Initial commit

---
+++ src/FindPrime.java
@@ -0,0 +1,25 @@
+public class FindPrime {
+    public static void main (String args[])
+       throws java.io.IOException {
+        int num;
+        boolean isPrime;
+        System.out.println("Enter the number");
+        num = (int) System.in.read();
+
+        if (num < 2)
+            isPrime = false;
+        else
+            isPrime = true;
+
+        for (int i=2; i <= num/i; i++){
+            if((num % i) == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime)
+            System.out.println("Simple number");
+        else
+            System.out.println("Don't simple number");
+    }
+}
